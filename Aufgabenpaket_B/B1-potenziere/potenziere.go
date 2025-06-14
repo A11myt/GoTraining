ea66ae48b82af925e7b6248ff66cc4f882fb170f
A11myt <jason.j@gmx.de>
2025-06-14 08:04:15 +0200

    added Lection 5

--- Aufgabenpaket_B/B1-potenziere/potenziere.go
+++ Aufgabenpaket_B/B1-potenziere/potenziere.go
@@ -4,9 +4,17 @@ package main
 // exponent muss >= 0 sein.
 
 func potenziere(basis, exponent int) int {
-	
 
+	var result = 1
 
+	if exponent == 0 {
+		return 1 // Jede Zahl hoch 0 ist 1
+	}
+
+	for i := 1; i <= exponent; i++ {
+		result *= basis
+
+	}
 	// bitte korrigieren Sie den Rumpf dieser Funktion.
-	return 0
+	return result
 }
