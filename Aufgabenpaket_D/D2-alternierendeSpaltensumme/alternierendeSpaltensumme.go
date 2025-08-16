762599da83abf65f4b047fc9e6931b3a71bcfb9c
A11myt <Jason.j@gmx.de>
2025-08-16 17:16:02 +0000

    added D2 & D3 solution

--- Aufgabenpaket_D/D2-alternierendeSpaltensumme/alternierendeSpaltensumme.go
+++ Aufgabenpaket_D/D2-alternierendeSpaltensumme/alternierendeSpaltensumme.go
@@ -4,6 +4,29 @@ package main
 // Zeile die alternierenden Spaltensummen stehen, korrigiert diese ggf.
 // und gibt die Anzahl der fehlerhaften Summen zurück.
 func alternierendeSpaltensumme(arr *[anzZeilen][anzSpalten]int) int {
+	var fehlerhafteSummen int = 0
+
+	// Iteriere über die Spalten, außer der letzten (die soll ja die Summe sein)
+	// for i := 0; i < anzSpalten-1; i++ {
+	for i := range arr[0][:anzSpalten] {
+		spaltensumme := 0 // Summe der aktuellen Spalte
+		// for j := 0; j < anzZeilen-1; j++ { // Letzte Zeile ignorieren
+		for j := range arr[:anzZeilen-1] {
+			if j%2 == 0 {
+				spaltensumme += arr[j][i]
+			} else {
+				spaltensumme -= arr[j][i]
+			}
+		}
+
+		// Prüfe, ob die Summe in der letzten Zeile korrekt ist
+		if spaltensumme != arr[anzZeilen-1][i] {
+			fehlerhafteSummen++                // Zähle fehlerhafte Summen
+			
+			//error hier
+			arr[anzZeilen-1][i] = spaltensumme // Korrigiere die Summe
+		}
+	}
 	// Bitte korrigieren Sie den Rumpf der Funktion.
-	return 99
+	return fehlerhafteSummen
 }
--- Aufgabenpaket_D/D3-mastermind/bewertung.go
+++ Aufgabenpaket_D/D3-mastermind/bewertung.go
@@ -2,7 +2,42 @@ package main
 
 // bewertung gibt die Bewertung von versuch an Hand von code zurück.
 // Die zurückgegebene Bewertung ist ein Paar (schwarze, weiße).
+// versuch und code sind Arrays der Länge 4, die Farben repräsentieren.
+
 func bewertung(versuch, code [anzStellen]string) (int, int) {
 	// bitte korrigieren Sie den Rumpf dieser Funktion.
-	return -1, -1
+	schwarze := 0
+	weiße := 0
+
+	// Zähle schwarze Treffer
+	for i, v := range versuch{
+		if v == code[i] {
+			schwarze++
+			// Markiere die Position als bereits gezählt
+			code[i] = "" // oder ein anderes Zeichen, das nicht in den Farben vorkommt
+			versuch[i] = "" // um zu verhindern, dass es später als weißer Treffer gezählt wird
+		}
+	}
+	// Zähle weiße Treffer
+	for _, v := range versuch {
+		for j, c := range code {
+			if v == c {
+				weiße++
+				// Markiere die Position als bereits gezählt
+				code[j] = "" // oder ein anderes Zeichen, das nicht in den Farben vorkommt
+				break // Beende die innere Schleife, um doppelte Zählung zu vermeiden
+			}
+		}
+	}
+	return schwarze, weiße
 }
+
+// ich habe ein Array von strings die stellen 6 verschiedene Farben da, A - F
+// und ich habe ein 4stelliges Array, das die Farben des Codes enthält
+// nun darf der spieler 4 farben raten, die in meinem Array gespeichert sind
+// ich muss nun die Farben vergleichen und die Anzahl der schwarzen und weißen Treffer zählen
+// schwarze Treffer sind die Farben, die an der richtigen Stelle geraten wurden
+// weiße Treffer sind die Farben, die zwar im Code enthalten sind, aber an der falschen
+// Stelle geraten wurden
+// ich muss also die Farben in den Arrays vergleichen und die Anzahl der Treffer zählen//
+	// auserdem gibt es nur eine bestimmte anzahl an versuchen, die zaehle ich
